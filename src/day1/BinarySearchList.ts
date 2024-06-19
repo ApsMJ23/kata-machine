3e4e155568914b88a74ad894f3d6cfd4fae0337f
Apurva Singh <apurvalion@gmail.com>
2024-06-20 02:43:39 +0530

    Implemented Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let hi = haystack.length;
+    do{
+        const mid = Math.floor(low+(hi-low)/2);
+        if(haystack[mid]===needle)return true;
+        if(haystack[mid]<needle){
+            low=mid+1;
+        }
+        else{
+            hi=mid;
+        }
+
+    }while(low<hi);
+    return false;
 
 }
